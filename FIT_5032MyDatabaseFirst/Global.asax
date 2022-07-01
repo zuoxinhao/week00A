f3862f0693995270a616da6f228ad66b5ed73b11
xinhao <3310507620@qq.com>
2022-07-01 17:41:09 +0800

    first attempt!

---
+++ FIT_5032MyDatabaseFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT_5032MyDatabaseFirst.MvcApplication" Language="C#" %>
---
+++ FIT_5032MyDatabaseFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT_5032MyDatabaseFirst.Startup))]
+namespace FIT_5032MyDatabaseFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
